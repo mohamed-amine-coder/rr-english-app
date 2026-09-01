49a846ff00380d9f8acf7694baafe95785cb0c82
mohamed-amine <rrcode@gmail.comr>
2026-09-01 16:48:50 +0200

    fdf

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'https://rrenglish.vercel.app/about' 
+        redirectTo: 'https://rrenglish.vercel.app/' 
       }
     });
 
